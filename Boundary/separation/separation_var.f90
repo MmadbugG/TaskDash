ba29972b58d725158ba2b7bb2c9d3c1fcf6a789c
MmadbugG <kapitsin.dan@gmail.com>
2019-04-01 09:49:31 +0300

    sep

--- Boundary/separation/separation_var.f90
+++ Boundary/separation/separation_var.f90
@@ -3,33 +3,66 @@ implicit none
 	real(8):: Bi, Fon, eps, h, ht
 	real(8), allocatable:: Td(:, :), xh(:), Fo(:)
 	integer:: i, j, n, nt
-        real(8):: fsl(3)=(/0.01, 0.1, 0.5/),&
+        real(8):: Ta, Te, alp, delt, c, rho, lam, a
+        real(8):: fsl(3)=(/0.05, 0.1, 0.5/),&
                   xsl(3)=(/0.0, 0.5, 1.0/)
 	
-	Bi = 1
-	Fon = 2
+	alp = 11000
+	c = 670
+	rho = 2500.0
+	lam = 0.74
+	Fon = 0.5
+	Ta = 20
+	Te = 100
+	delt = 0.005
 	
-	n = 61 !nodes length, the length of interval is equal to n - 1
-	nt = 40
+	Bi = alp* delt/ lam
+	a = lam/ (c* rho)
+	
+	n = 100 !nodes length, the length of interval is equal to n - 1
+	nt = 100
 	allocate(xh(n), Fo(nt), Td(n,nt))
-	ht = Fon/ nt
+	ht = Fon/ (nt - 1)
 	h = 1.0/ (n - 1)
 	eps = 1.0e-6
         do i = 1, n
-                xh(i) = (i-1)* h
+                xh(i) = (i - 1)* h
         end do
 
 	do i = 1, nt
-	        Fo(i) = i* ht
+	        Fo(i) = (i - 1)* ht
 	        do j = 1, n
 	                Td(j,i) = series_T(eps, Bi, Fo(i), xh(j))
 	        end do
 	end do
+	
+	Td(:, 1) = 1
+	call units(Td, Fo, xh, n, nt, a, delt, Ta, Te)
+	do i = 1, 3
+	        fsl(i) = fsl(i)* delt**2 / a
+	        xsl(i) = xsl(i)* delt
+	end do 
 	call export_txt(Td, Bi, Fo, xh, Fon, n, nt)
         call export_fo(xh, Fo, Td, n, nt, fsl)
         call export_xh(xh, Fo, Td, n, nt, xsl)
+               
 
 contains
+subroutine units(Td, Fo, xh, n, nt, a, delt, Ta, Te)
+        real(8):: Ta, Te, a, delt
+        real(8):: Td(n, nt), xh(n), Fo(nt)
+        integer:: n, nt, i, j
+        
+        do j = 1, nt
+                Fo(j) = delt**2 / a* Fo(j)
+        end do
+        do i = 1, n
+                do j = 1, nt
+                        Td(i,j) = Td(i,j)* (Ta - Te) + Te
+                end do
+                xh(i) = xh(i)* delt
+        end do
+end subroutine
 real(8) function series_T(eps, Bi, Fo, xh)
 	integer:: n
 	real(8):: eps, Bi, Fo, xh
@@ -106,10 +139,14 @@ integer function slice(xh, xsl, n)
         integer:: isl, n
         real(8):: dist, buff, xsl
         integer:: i
-        buff = 1.0
+        buff = abs(xh(n)-xh(1)) + 1
         do i = 1, n
                 dist = abs(xh(i) - xsl)
                 if (dist > buff) then
+                        if (i == 1) then
+                                write(*,*) 'slice bad...'
+                                stop
+                        end if
                         exit
                 end if
                 buff = dist
@@ -155,6 +192,7 @@ subroutine export_xh(xh, Fo, Td, n, nt, xsl)
                 isl(i) = slice(xh, xsl(i), n)
                 write(1, *) Td(isl(i), :)
         end do
+        
         close(1)
 end subroutine
 end program
