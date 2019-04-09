9240a5e982dc8527b89156f82e2f73c87d30bef0
MmadbugG <kapitsin.dan@gmail.com>
2019-04-10 00:19:16 +0300

    turb

--- Flow/turbulense.f90
+++ Flow/turbulense.f90
@@ -2,49 +2,39 @@ program turb
 implicit none
         integer:: i, j, n
         real(8), allocatable:: x(:), v(:), h(:), dv(:), mu(:)
-        real(8), allocatable:: A(:), B(:), C(:), F(:)
-        real(8):: nu, rho, k, p, v0, vn
+        real(8), allocatable:: A(:), B(:), C(:), F(:), vplus(:), xplus(:)
+        real(8):: nu, rho, p, v0, vn, q, h1
         
         rho = 1.293
         nu = 13.28
-        n = 101
-	k = 0.9
-	mu(:) = rho* nu
+	q = 1.1 ! q > 1
+	h1 = 0.001
+
+	n = (int(log((h1* 2 - 1 + q)/ (2.0* h1))/ log(q)) + 1)* 2 + 1
 	
 	allocate(h(n-1), A(n-2), B(n-2), C(n-2), F(n-2), v(n), &
-	x(n), dv(n))
-		
-	call grid(n-1, k, h)
-	stop
-	n = n - 1
-	x(n/2) = 0.5
-	do i = n/2+1, n-1
-		x(i) = h(n/2)* ((1 - k** (i-n/2))/ (1 - k)) + 0.5
-		x(n-i) = 1 - x(i)
-	end do
-	n = n + 1
-	write(*,*) n
-	x(1) = 0.0
-	x(n) = 1.0
-	stop
-	do i = 1, n
-	write(*,*) x(i)
-	end do
-	stop
+	x(n), dv(n), mu(n), vplus(n), xplus(n))
 	
-	
-	p = 500
+	call grid(n, q, h1, h, x)
+        
+	p = 45
 	v0 = 0
 	vn = 1
-	
-	call matrix(h, p, mu, v0, vn, n-2, A, B, C, F)
-	call sweep(A, B, C, F, n-2, v)
+	mu(:) = rho* nu
 	v(1) = v0
 	v(n) = vn
-
 	
-        call deriv(v, h, dv, n)
-        call calc_mu(nu, rho, x, dv, v, n, mu)
+	do i = 1, 1000
+        	call matrix(h, p, mu, v0, vn, n-2, A, B, C, F)
+        	call sweep(A, B, C, F, n-2, v)
+                call deriv(v, h, dv, n)
+                call calc_mu(nu, rho, x, dv, v, n, mu, vplus, xplus)
+        end do
+        open(1, file="turb_out_plus.txt")
+        do i = 1, n
+        write(1,*) log(xplus(i)), vplus(i)
+        end do
+        close(1)
 
 contains
 subroutine deriv(v, h, dv, n)
@@ -57,35 +47,55 @@ subroutine deriv(v, h, dv, n)
                 dv(i) = (v(i+1) - v(i))/ h(i)
         end do      
 end subroutine
-subroutine calc_mu(nu, rho, x, dv, v, n, mu)
+subroutine calc_mu(nu, rho, x, dv, v, n, mu, vplus, xplus)
         integer:: n, i
-        real(8):: rho, nu, muo, mut, k, demf, l, nut
+        real(8):: rho, nu, muo, mut, k, dempf, l, nut, &
+        vt, tau, vplus(n), xplus(n), aa        
         real(8):: x(n), dv(n), v(n), mu(n)
+        
         
         muo = rho* nu
         k = 0.41
-        demf = 1
+        aa = 26
         do i = 1, n
+                tau = nu* abs(dv(i))
+                vt = sqrt(tau/ rho)
+                vplus(i) = v(i)/ vt
+                xplus(i) = x(i)* vt/ nu
+                dempf = (1 - exp(xplus(i)/aa))**2
                 l = k* x(i)
-                nut = demf* l** 2* dv(i)
+                nut = dempf* l** 2* abs(dv(i))
                 mut = nut* rho
                 mu(i) = muo + mut
         end do
 end subroutine
 !_______________________________
 
-subroutine grid(n, k, h)
+subroutine grid(n, q, h1, h, x)
 	integer:: n
-	real(8):: k
-	real(8):: h(n)
+	real(8):: q, h1
+	real(8):: h(n-1), x(n)
 	integer:: i
-
-	h(n/2+1) = 0.5* (1 - k)/ (1 - k** (n/2))
-	h(n/2) = h(n/2+1)
-	do i = n/2+1, n-1
-		h(i+1) = h(i)* k
-		h(n-i) = h(i+1)
+	
+	i = 1
+	x(1) = 0
+	x(2) = h1
+	h(1) = h1
+	do while(x(i+1) <= 0.5)
+	        i = i + 1 
+	        h(i) = h(i-1)* q
+	        x(i+1) = x(i) + h(i)
 	end do
+	q = 0.5/x(i+1)
+	do i = 2, n/2
+	        h(i) = h(i)* q
+	        x(i+1) = x(i) + h(i)
+	end do
+	do i = 1, n/2
+	        h(n-i) = h(i)
+	        x(n-i+1) = 1 - x(i)
+	end do
+	x(n/2 + 1) = 0.5
 end subroutine
 subroutine matrix(h, p, mu, y0, yn, n, A, B, C, F)
 	real(8):: h(n+1), mu(n+2), y0, yn, p
@@ -100,17 +110,17 @@ subroutine matrix(h, p, mu, y0, yn, n, A, B, C, F)
 		F(i) = p/ mu(i+1)
 	end do
 	F(1) = p/ mu(2) - y0* 2/ (h(1)* (h(1) + h(2)) )
-	F(n) = p/ mu(n+1) - yn* 2/ (h(n)* (h(n) + h(n-1)) )
+	F(n) = p/ mu(n+1) - yn* 2/ (h(n+1)* (h(n+1) + h(n)) )
 	A(1) = 0
 	B(n) = 0
 end subroutine
-subroutine sweep(A, B, C, F, n, y)
+subroutine sweep(A, B, C, F, n, v)
 	real(8):: A(n), B(n), C(n), F(n)
 	integer:: n
-	real(8):: y(n+2)
+	real(8):: v(n+2)
 	real(8):: alp(n), bet(n)
 	integer:: i
-	
+
 	alp(2) = - B(1)/ C(1)
 	bet(2) = F(1)/ C(1)
 
@@ -120,9 +130,9 @@ subroutine sweep(A, B, C, F, n, y)
         	(A(i)* alp(i) + C(i))
 	end do
 
-	y(n+1) = (F(n) - A(n)* bet(n))/ (C(n) + A(n)* alp(n))
+	v(n+1) = (F(n) - A(n)* bet(n))/ (C(n) + A(n)* alp(n))
 	do i = n-1 , 1, -1
-		y(i+1) = alp(i+1)* y(i+1) + bet(i+1)
+		v(i+1) = alp(i+1)* v(i+2) + bet(i+1)
 	end do
 end subroutine
 end program
