1773c4618eb3be74eb5360f105995aada116f995
MmadbugG <kapitsin.dan@gmail.com>
2019-04-11 08:19:13 +0300

    airtype

--- Air/air.f90
+++ Air/air.f90
@@ -3,24 +3,63 @@ implicit none
                      
 contains
 subroutine calc_curve(r1, r2, alp, bet, gam, o1o2, curve, n)
-        real:: r1, r2, alp, bet, gam, o1o2
+        real, intent(in):: r1, r2, alp, bet, gam, o1o2
         integer:: i, j, n
         real:: curve(n)
-        real:: tet(4), p(4), po(4), m(2,2), ab, fi, rs, Rr1, Rr2
+        real:: tet(4), p(4), po(4), m (2,2), fi, rs, Rr1, Rr2, &
+	ab, star, a(2), b(2)
         type circle
                 real r
                 real o(2)
         end type
-        type(circle):: c
-        
+        type(circle):: o1, o2
+	o1%r = 4
+	o1%o = 0
+	a = 2
+	b = func(o1, a(1))
+	write(*,*) b
+        star = r1* cos(alp) - r2* cos(bet)
+	fi = acos(star/ o1o2)
         rs = ab/ (2* sin((alp + bet)/ 2))
         Rr1 = rs* sin(gam/ 2)/ sin((alp - bet + gam)/ 2)
         Rr2 = rs* sin((alp + bet - gam)/ 2)/ sin(bet - gam/ 2)
 
 end subroutine
-real function vectorangle(v1,v2)
-        real:: v1(2), v2(2)
-        
+function func(circ, fi) result(v)
+	real:: fi
+	type circle
+                real r
+                real o(2)
+        end type
+	type(circle):: circ
+	real:: v(2)
+	v(1) = fi
+	v(2) = circ%r
+
+end function	
+real function vectorangle(v1, v2, n)
+        real:: v1(n), v2(n)
+	integer:: n
+	vectorangle = acos(scal(v1, v2, n)&
+	/ (norm(v1, n)* norm(v2, n)))       
+end function
+real function scal(v1, v2, n)
+	real:: v1(n), v2(n), s
+	integer:: n, i
+	s = 0
+	do i = 1, n
+		s = s + v1(i)* v2(i)
+	end do
+	scal = s
+end function
+real function norm(v, n)
+	integer:: n, i
+	real:: s, v(n)
+	s = 0
+	do i = 1, n
+		s = s + v(i)**2
+	end do
+	norm = sqrt(s)
 end function
 end module
 
