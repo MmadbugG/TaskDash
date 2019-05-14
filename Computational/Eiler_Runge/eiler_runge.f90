65e7631f489ed77af170af210ef724d2dd89b643
MmadbugG <kapitsin.dan@gmail.com>
2019-05-15 00:03:15 +0300

    eiler_runge_v1

--- Computational/Eiler_Runge/eiler_runge.f90
+++ Computational/Eiler_Runge/eiler_runge.f90
@@ -1,9 +1,8 @@
 program pract2
 implicit none
-real:: x0, a, tmax, h
-real, allocatable:: x(:), t(:)
-integer:: N,i,m
-real:: k
+real, allocatable:: x(:), y(:)
+integer:: N, i, m
+real:: k1, k2, k3, eps, a, b, y0, h
 
 !Открывам файл с исходными данными и создаем файл для выходных данных
 !Open the file with the original values and create a file for the output values
@@ -14,34 +13,37 @@ open(2, file='out2.txt', status='replace')
 !Ввод параметров задачи
 !input of task parameters
 
-read(1,*) a, x0, tmax, h
+read(1,*) a, b, y0, eps
 
 !Находим число шагов и создаем массив нужного размера
 !find the number of steps and create an array of the correct size
-
-N = aint(tmax/h + 2.0)
-allocate(x(N), t(N))
+h = 0.01
+N = aint((b - a)/ h + 2.0)
+allocate(x(N), y(N))
 
 !Находим значения x и t по формуле явного метода Рунге-Кутты 2-го порядка или Эйлера
-!find the values ​​of x and t by the formula of the explicit Runge-Kutta method of the second order or Euler method
+!find the values of x and t by the formula of the explicit Runge-Kutta method of the second order or Euler method
 
-x(1) = x0
-t(1) = 0
+x(1) = a
+y(1) = y0
+
 write(*,*)'Use Euler method - press 2, use Runge-Kutta method - press 1'
 read(*,*)m
 if(m==1)then
-    do i = 2,N
-    k = x(i-1) + h/2 * f(x(i-1), a)
-    x(i) = x(i-1) + h* f(k, a)
-    t(i) = (i-1) * h
-    write(2,*) i,t(i), x(i)
+    do i = 1,N-1
+          k1 = f(x(i), y(i))
+          k2 = f(x(i) + h/2.0, y(i) + h/2.0 *k1)
+          k3 = f(x(i) + h, y(i) - h* k1 + 2.0* h* k2)
+          y(i+1) = y(i) + h/6.0* (k1 + 4*k2 + k3) 
+          x(i+1) = i* h
+          write(2,*) i, y(i+1), x(i+1)
     end do
 endif
 if(m==2)then
     do i = 2,N
-    x(i) = x(i-1) + h* f(x(i-1), a)
-    t(i) = (i-1) * h
-    write(2,*) i,t(i),x(i)
+    y(i) = y(i-1) + h* f(x(i-1), y(i-1))
+    x(i) = (i-1) * h
+    write(2,*) i, y(i), x(i)
     end do
 end if
 
@@ -49,8 +51,8 @@ end if
 !Description of the function f
 
 contains
-real function f(x, a)
-real, intent(in):: x, a
-   f = a * sqrt(x)
+real function f(x, y)
+real, intent(in):: x, y
+   f = (4*x + 2*y)/(2*x + 1)
 end function
 end
